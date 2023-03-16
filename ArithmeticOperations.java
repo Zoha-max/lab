661f76ea58a7e0b7ead17e04c6e40c376d71bdf1
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-03-16 14:05:30 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a=10, b=23, sum, sub, mul,div;
+        sum = a + b;
+        sub = a - b;
+        mul = a * b;
+        div = a % b;
+ 
+        System.out.println("Sum : " + sum);
+        System.out.println("substraction : " + sub);
+        System.out.println("multiplication : " + mul);
+        System.out.println("Remainder : " + div);
+    }
+}
