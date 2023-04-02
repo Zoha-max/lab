4916c97c90b4a8e65f9d7edbd305227263c2378a
Zoha-max <127471377+Zoha-max@users.noreply.github.com>
2023-04-02 13:23:12 +0530

    Add files via upload

---
+++ Leap_Year.java
@@ -0,0 +1,12 @@
+class Leap_Year{
+public static void main(String args[]){
+
+int a = 2042;
+
+if(a % 4==0){
+System.out.println(a +" is a leap year.");
+}
+else
+{
+System.out.println(a +" is not a leap year.");
+}}}
